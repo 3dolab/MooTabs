4a4ff26814d58c47337264a0f7ce91722c2818e0
3dolab <info@3dolab.net>
2011-07-14 12:40:50 -0700

    smooth animation for next/prev loop, tab controls using totalTabs ("panes") option

--- Source/mootabs-0.1.0-nc.js
+++ Source/mootabs-0.1.0-nc.js
@@ -29,7 +29,7 @@ var MooTabs = new Class({
         autoPlay: true,
         autoPlayWait: 10000,
         totalTabs: 1,
-	loop: true
+	loop: false
     },
 
     initialize: function(tabs, contents, options) {
@@ -37,6 +37,8 @@ var MooTabs = new Class({
 
         this.tabsElement = tabs[0];
         this.contentsElement = contents[0];
+	
+	this.initialWidth = this.contentsElement.getStyle('width').toInt();
 
         this.tabsList = this.tabsElement.getChildren('li');
         this.contentsList = this.contentsElement.getChildren('li');
@@ -68,7 +70,7 @@ var MooTabs = new Class({
 
         this.tabsList.each(function(tab, i) {
             this.setupTabs(tab, this.contentsList[i], i);
-	    this.contentsList[i].setProperty('id', i);
+	    //this.contentsList[i].setProperty('id', i);
         }, this);
 
         if (this.options.autoPlay) {
@@ -79,17 +81,36 @@ var MooTabs = new Class({
     setupTabs: function(tab, contents, i) {
         tab.addEvent('mousedown', function(e) {
             if (tab != this.activeTab) {
+		//new Event(e).stop();
+		this.stop();
                 //this.stop().play();
-                this.activeTab.removeClass(this.options.activeClass);
-                this.activeTab = tab;
-                this.activeTab.addClass(this.options.activeClass);
+		this.activeTab.removeClass(this.options.activeClass);
+		if (this.options.totalTabs !=1){
+			if (i < this.tabsCount-1){
+				this.activeTab = tab;
+			}else {
+				this.activeTab = this.tabsList[this.tabsCount-1];
+				i = this.tabsCount-1;
+			}
+		} else {                
+			this.activeTab = tab;                
+		}
+		this.activeTab.addClass(this.options.activeClass);
+		var d = (i - this.currentIndex) * this.windowWidth;
 
-                var d = (i - this.currentIndex) * this.windowWidth;
-
-                this.currentPosition -= d;
+		this.currentPosition -= d;
 		var currentPos = this.currentPosition;
                 this.slideFx.start({
                         left: this.currentPosition + 'px'
+                        /*
+                }).chain(function() {
+		  	    if (this.options.loop = true){
+		var tabMargin = tabsContainer.getFirst('li').getStyle('margin-left').toInt();
+		alert(tabMargin+d);
+		tabsContainer.getFirst('li').dispose();
+		tabsContainer.getFirst('li').setStyle('margin-left', -currentPos);
+		alert('dispose');
+		}*/
 		});
 
 
@@ -115,62 +136,136 @@ var MooTabs = new Class({
     },
 
     nextSlide: function() {
-      	    if (this.options.loop = true){
-	      this.forward(this.tabsList[this.currentIndex], this.contentsList, this.currentIndex);
-	      return this;
-	    }
-	    
+	if (this.options.loop = true){
+		this.forward(this.tabsList[this.currentIndex], this.contentsList, this.currentIndex);
+		return this;
+	}
         if (this.currentIndex == this.tabsCount-1) {
-	    this.tabsList[0].fireEvent('mousedown');
+		this.tabsList[0].fireEvent('mousedown');
         } else {
-            this.tabsList[this.currentIndex+1].fireEvent('mousedown');
+		this.tabsList[this.currentIndex+1].fireEvent('mousedown');
         }
         return this;
     },
 
     previousSlide: function() {
-            if (this.options.loop = true){
-	      this.backward(this.tabsList[this.currentIndex], this.contentsList, this.currentIndex);
-	      return this;
-	    }
+	if (this.options.loop = true){
+		this.backward(this.tabsList[this.currentIndex], this.contentsList, this.currentIndex);
+		return this;
+	}
         if (this.currentIndex == 0) this.tabsList[this.tabsCount-1].fireEvent('mousedown');
         else this.tabsList[this.currentIndex-1].fireEvent('mousedown');
         return this;
     },
 
     forward:  function(tab, contentlist, i) {
+	this.stop();
         var d = this.windowWidth;
 	this.currentPosition -= d;
-
-	      this.contentsElement.setStyle('width', (this.contentsElement.getStyle('width').toInt()+d));
-	      this.contentsElement.getFirst('li').setStyle('margin-left', 0);
-	      this.contentsElement.getLast('li').setStyle('margin-right', 0);
+	if (this.currentPosition >= 0){
+	    if (this.contentsElement.getStyle('width').toInt() > this.initialWidth)
+		this.contentsElement.setStyle('width', (this.contentsElement.getStyle('width').toInt()-d));
+	    else
+		this.contentsElement.setStyle('width', this.initialWidth);
+	}else{
+		this.contentsElement.setStyle('width', (this.contentsElement.getStyle('width').toInt()+d));
+	}	      
+	var firstmargin = this.contentsElement.getLast('li').getStyle('margin-left').toInt();
+	if (firstmargin >= 0){
+		firstmargin = -d;
+	}
 	      this.contentsElement.getFirst('li').inject(this.contentsElement, 'bottom');
-	      this.contentsElement.getFirst('li').setStyle('margin-left', -this.currentPosition);
+	      this.contentsElement.getLast('li').clone().inject(this.contentsElement, 'top');
+	if (this.currentPosition >= 0)
+	      this.contentsElement.getLast('li').setStyle('margin-left', firstmargin+d);
+	else if (this.currentPosition < 0)
+	      this.contentsElement.getLast('li').setStyle('margin-left', 0);
 
+	      var currentPos = this.currentPosition;
+	      var tabsContainer = this.contentsElement;
 	this.slideFx.start({
                         left: this.currentPosition + 'px'
+		}).chain(function() {
+		tabsContainer.getFirst('li').dispose();
+		tabsContainer.getFirst('li').setStyle('margin-left', -currentPos);
 		});
+
 	this.currentIndex = i;
-        this.fireEvent('change', [tab, contents[i]]);
+        this.fireEvent('change', [tab, contentlist[i]]);
         return this;
     },
 			
     backward:   function(tab, contentlist, i) {
+	this.stop();
         var d = -this.windowWidth;
 	this.currentPosition -= d;
-
-	      this.contentsElement.setStyle('width', (this.contentsElement.getStyle('width').toInt()+d));
-	      this.contentsElement.getFirst('li').setStyle('margin-left', 0);
+	      var firstmargin = this.contentsElement.getFirst('li').getStyle('margin-left').toInt();
+	if (this.currentPosition > 0){
+		this.contentsElement.setStyle('width', (this.contentsElement.getStyle('width').toInt()-d));
+	}
 	      this.contentsElement.getLast('li').inject(this.contentsElement, 'top');
-	      this.contentsElement.getFirst('li').setStyle('margin-left', -this.currentPosition);
-
+	      this.contentsElement.getFirst('li').clone().inject(this.contentsElement, 'bottom');
+	      this.contentsElement.getFirst('li').setStyle('margin-left', firstmargin+d);
+	if (this.currentPosition <= 0)
+	      this.contentsElement.getFirst('li').getNext('li').setStyle('margin-left', 0);
+  
+	      var initialWidth = this.initialWidth;
+	      var currentPos = this.currentPosition;
+	      var tabsContainer = this.contentsElement;
 	this.slideFx.start({
                         left: this.currentPosition + 'px'
+		}).chain(function() {
+		tabsContainer.getLast('li').dispose();
+		tabsContainer.getFirst('li').setStyle('margin-left', -currentPos);
+	if (currentPos <= 0){
+	    if (tabsContainer.getStyle('width').toInt() > initialWidth)
+		tabsContainer.setStyle('width', (tabsContainer.getStyle('width').toInt()+d));
+	    else
+		tabsContainer.setStyle('width', initialWidth);
+	    }
 		});
+
 	this.currentIndex = i;
-        this.fireEvent('change', [tab, contents[i]]);
+        this.fireEvent('change', [tab, contentlist[i]]);
+        return this;
+    },
+    backwardbackup:   function(tab, contentlist, i) {
+	this.stop();
+        var d = -this.windowWidth;
+	this.currentPosition -= d;
+	      var firstmargin = this.contentsElement.getFirst('li').getStyle('margin-left').toInt();
+	if (this.currentPosition > 0){
+		this.contentsElement.setStyle('width', (this.contentsElement.getStyle('width').toInt()-d));
+	}
+
+	this.contentsElement.getLast('li').setStyle('margin-right', 0);
+	this.contentsElement.getLast('li').inject(this.contentsElement, 'top');
+	this.contentsElement.getFirst('li').clone().inject(this.contentsElement, 'bottom');
+	if (this.currentPosition <= 0)
+		this.contentsElement.getFirst('li').setStyle('margin-left', firstmargin+d);
+	else
+		this.contentsElement.getFirst('li').getNext('li').setStyle('margin-left', 0);
+  
+	var initialWidth = this.initialWidth;
+	var currentPos = this.currentPosition;
+	var tabsContainer = this.contentsElement;
+	this.slideFx.start({
+                        left: this.currentPosition + 'px'
+	}).chain(function() {
+		tabsContainer.getLast('li').dispose();
+		if (currentPos <= 0){
+			tabsContainer.getFirst('li').setStyle('margin-left', -currentPos);
+			if (tabsContainer.getStyle('width').toInt() > initialWidth)
+				tabsContainer.setStyle('width', (tabsContainer.getStyle('width').toInt()+d));
+			else
+				tabsContainer.setStyle('width', initialWidth);
+		} else {
+			tabsContainer.getLast('li').setStyle('margin-right', currentPos);
+		}	    
+	});
+
+	this.currentIndex = i;
+        this.fireEvent('change', [tab, contentlist[i]]);
         return this;
     }
-
 });
